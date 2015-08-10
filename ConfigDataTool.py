2b2f02d93bf8b707ebe26676c036577a221711a8
Matri <matrxdm@126.cm>
2015-08-10 18:25:30 +0800

    add ConfigDataTool.py

---
+++ ConfigDataTool.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# @Date    : 2015-08-10 17:25:01
+# @Author  : MatriQ (Matrixdom@126.com)
+# @Link    : https://github.com/MatriQ
+# @Version : 0.1
+
+import os
+import xlrd
+
+def main():
+	pass
+
+if __name__ == '__main__':
+	main()
