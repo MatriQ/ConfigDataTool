abaaeb85dcbd7f0bcecf90fc2629260ff3b1b956
Matri <matrxdm@126.cm>
2015-08-10 18:40:35 +0800

    add method getNeedCols

--- ConfigDataTool.py
+++ ConfigDataTool.py
@@ -6,10 +6,32 @@
 # @Version : 0.1
 
 import os
+import path from os.path
 import xlrd
 
+# get need gene data cols
+def getNeedCols(table):
+	cols=[]
+	ncols = table.ncols
+	for i in range(ncols ):
+		if table.cell(3,i).value >= 2:
+				cols.append(i)
+	return cols
+
 def main():
-	pass
+	if len(sys.argv) < 1 :
+		raise Exception("argv can not be empty")
+	fileName=sys.argv[1]
+	filePath=os.path.abspath(fileName)
+	try:
+		data = xlrd.open_workbook(filePath)
+		table = data.sheets()[0]          #通过索引顺序获取
+		cols=getNeedCols(table)
+		
+		
+	except Exception, e:
+		raise e
+
 
 if __name__ == '__main__':
 	main()
