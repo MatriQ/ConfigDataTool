4fec6523ad200bf938bbb9f59ba975839fb4f1f7
MatriQi <matrixdom@126.com>
2015-08-11 10:19:08 +0800

    add CreateAllConfig.py

---
+++ CreateAllConfig.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# @Date    : 2015-08-11 10:25:01
+# @Author  : MatriQ (Matrixdom@126.com)
+# @Link    : https://github.com/MatriQ
+# @Version : 0.1
+
+import os
+import sys
